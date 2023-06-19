f59386b1016385f1452689b85861143fdbec4fe9
jiwookim7211 <108187456+jiwookim7211@users.noreply.github.com>
2023-06-20 03:11:01 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const router = require('express').Router();
+const express = require('express');
+const app = express();
 
-router.get('/',(req,res,next)=>{
-    res.render('index',{
-        javascriptkey:process.env.javascriptkey
-    });
+app.listen(8080, function(){
+    console.log('listining on 8080')
 });
-
-module.exports = router;
-(.env)
-
-javascriptkey=6304186388c4dcaf04316228175dda24
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+});
+app.use(express.static('public'));
